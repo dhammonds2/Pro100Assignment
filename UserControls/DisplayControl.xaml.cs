e34e65d285de3d6fe065a3f4798949388ff1ba24
wmcentire <wmcentire@student.neumont.edu>
2022-11-29 23:05:35 -0700

    Created the user control DisplayControl, used to output stored drone data.

---
+++ UserControls/DisplayControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectAstroClass.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DisplayControl.xaml
+    /// </summary>
+    public partial class DisplayControl : UserControl
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int DispWidth { get; set; } = 80;
+        public int DispHeight { get; set; } = 30;
+
+        public string TitleWidth { get; set; } = "Auto";
+
+        public DisplayControl()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
